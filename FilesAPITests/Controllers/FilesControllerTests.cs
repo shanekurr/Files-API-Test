074b5c59ef56e7ebce4e1cd7cb58adbeefd8f82c
Shane Kurr <skurr@fieldlevel.com>
2023-08-29 22:23:46 -0700

    Add unit test project and PostTest

---
+++ FilesAPITests/Controllers/FilesControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace FilesAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class FilesControllerTests
+    {
+        [TestMethod()]
+        public async Task PostTest()
+        {
+            System.IO.File.WriteAllText("testfile.txt", "This is a test file.");
+            var controller = new FilesController();
+            using (var stream = System.IO.File.OpenRead("testfile.txt"))
+            {
+                var file = new FormFile(stream, 0, stream.Length, null, Path.GetFileName(stream.Name))
+                {
+                    Headers = new HeaderDictionary(),
+                    ContentType = "text/plain"
+                };
+                var actionResult = await controller.Post(file);
+                Assert.IsNotNull(actionResult);
+
+                var result = actionResult as CreatedAtRouteResult;
+                Assert.IsNotNull(result);
+
+                var fileResult = result.Value as File;
+                Assert.IsNotNull(fileResult);
+
+                Assert.IsTrue(fileResult.Id > 0);
+
+                await controller.Delete(fileResult.Id);
+            }
+        }
+    }
+}
