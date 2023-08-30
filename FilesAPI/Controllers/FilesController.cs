6970bf3aa2492903fb77ec027add1efdbab22851
Shane Kurr <skurr@fieldlevel.com>
2023-08-29 20:33:27 -0700

    Bug fix, add update, list and delete endpoints

--- FilesAPI/Controllers/FilesController.cs
+++ FilesAPI/Controllers/FilesController.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FilesAPI.Controllers
 {
@@ -18,7 +19,8 @@ namespace FilesAPI.Controllers
         [ProducesResponseType(typeof(FilesAPI.File), StatusCodes.Status200OK)]
         public IActionResult Get(int fileId)
         {
-            return _context.Files.Find(fileId) is File file ? Ok(file) : NotFound();
+            var file = _context.Files.Include(t => t.Versions).FirstOrDefault(f => f.Id == fileId);
+            return file != null ? Ok(file) : NotFound();
         }
         
         [HttpPost]
@@ -49,6 +51,70 @@ namespace FilesAPI.Controllers
             await _context.FileVersions.AddAsync(newVersion);
             await _context.SaveChangesAsync();
             return CreatedAtRoute("GetFile", new { fileId = newFile.Id }, newFile);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(FilesAPI.File), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Put(int fileId, IFormFile file)
+        {
+            var existingFile = await _context.Files.FindAsync(fileId);
+            if (existingFile == null)
+            {
+                return NotFound();
+            }
+            var path = System.IO.Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                Guid.NewGuid().ToString());
+            using var stream = System.IO.File.Create(path);
+            await file.CopyToAsync(stream);
+            var newVersion = new FileVersion
+            {
+                FileId = existingFile.Id,
+                Version = existingFile.Versions.Max(t => t.Version) + 1,
+                Path = path,
+                Created = DateTime.UtcNow
+            };
+            _context.FileVersions.Add(newVersion);
+            existingFile.Modified = DateTime.UtcNow;
+            existingFile = _context.Files.Update(existingFile).Entity;
+            await _context.SaveChangesAsync();
+            return Ok(existingFile);
+        }
+
+        [HttpGet("", Name = "GetFiles")]
+        [ProducesResponseType(typeof(IEnumerable<FilesAPI.File>), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            var files = _context.Files.Include(t => t.Versions).ToList();
+            return Ok(files);
+        }
+
+        
+        [HttpDelete("{fileId}", Name = "DeleteFile")]
+        [ProducesResponseType(typeof(FilesAPI.File), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete(int fileId)
+        {
+            var file = await _context.Files.FindAsync(fileId);
+            if (file == null)
+            {
+                return NotFound();
+            }
+            var versions = _context.FileVersions.Where(v => v.FileId == fileId);
+            foreach (var fileVersion in versions)
+            {
+                try
+                {
+                    System.IO.File.Delete(fileVersion.Path);
+                }
+                catch(Exception e)
+                {
+                    //TODO: log error
+                }
+            }
+            
+            _context.Files.Remove(file);
+            await _context.SaveChangesAsync();
+            return Ok(file);
         }
     }
 }
