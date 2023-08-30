951c71c1342f0f23d2afe3b99325e9bb009bafe2
Shane Kurr <skurr@fieldlevel.com>
2023-08-29 19:51:47 -0700

    Add logic to Get endpoint, add Post endpoint

--- FilesAPI/Controllers/FilesController.cs
+++ FilesAPI/Controllers/FilesController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilesAPI.Controllers
@@ -7,18 +7,48 @@ namespace FilesAPI.Controllers
     [ApiController]
     public class FilesController : ControllerBase
     {
-        
+        private readonly FilesContext _context;
 
         public FilesController()
         {
-            
+            _context = new FilesContext();
         }
 
-        [HttpGet("{fileId}",Name = "GetFile")]
+        [HttpGet("{fileId}", Name = "GetFile")]
+        [ProducesResponseType(typeof(FilesAPI.File), StatusCodes.Status200OK)]
         public IActionResult Get(int fileId)
         {
-
-            return Ok();
+            return _context.Files.Find(fileId) is File file ? Ok(file) : NotFound();
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(typeof(FilesAPI.File), StatusCodes.Status201Created)]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            var newFile = new File
+            {
+                Name = file.FileName,
+                ContentType = file.ContentType,
+                Created = DateTime.UtcNow,
+                Modified = DateTime.UtcNow
+            };
+            await _context.Files.AddAsync(newFile);
+            await _context.SaveChangesAsync();
+            var path = System.IO.Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                Guid.NewGuid().ToString());
+            await using var stream = System.IO.File.Create(path);
+            await file.CopyToAsync(stream);
+            var newVersion = new FileVersion
+            {
+                FileId = newFile.Id,
+                Version = 1,
+                Path = path,
+                Created = DateTime.UtcNow
+            };
+            await _context.FileVersions.AddAsync(newVersion);
+            await _context.SaveChangesAsync();
+            return CreatedAtRoute("GetFile", new { fileId = newFile.Id }, newFile);
         }
     }
 }
